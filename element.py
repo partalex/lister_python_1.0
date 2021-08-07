f93102623074ff2a078122930099ae4c21080cb2
Aleksandar Vasilic <48293920+babazorka@users.noreply.github.com>
2021-08-07 11:02:39 +0200

    neko sredjivanje

--- element.py
+++ element.py
@@ -25,17 +25,14 @@ class Element:
     duzni_metar_materijala = 0
     duzni_metar_trake = 0
 
-    def __init__(self, line):
-        if line[0] not in lista.Lista.ulaz_csv.keys():
-            lista.Lista.ulaz_csv[line[0]] = sablon
-
+    def __procitaj_oznaka_duzina_sirina_debljina_broj_elemenata(self, line):
         self.oznaka = line[1]
         self.duzina = int(line[2][0: -3])
         self.sirina = int(line[3][0: -3])
         self.debljina = int(line[4][0: -3])
-
         self.broj_elemenata = int(line[5])
 
+    def __odredi_kantove(self):
         kant = re.search("_kant_.+$", self.oznaka)
         if kant is not None:
             kant = kant.group()
@@ -54,6 +51,7 @@ class Element:
                     sirina_kant = sirina_kant.group()
                     self.broj_kantovanih_sirina = int(sirina_kant[-1])
 
+    def __uredi_statistiku(self, line):
         self.duzni_metar_materijala = self.duzina * self.broj_elemenata / 1000
         self.kvadratura_materijala = self.duzina * self.sirina * self.broj_elemenata
         self.kvadratura_materijala /= 1000000
@@ -61,52 +59,15 @@ class Element:
         self.duzni_metar_trake *= self.broj_elemenata
         self.duzni_metar_trake /= 1000
 
-    # def __init__(self, string):
-    #     lista = string.split(',')
-    #
-    #     self.materijal = lista[0][1:-1]
-    #     self.oznaka = lista[1][1:-1]
-    #     generic = re.search("^generic_", self.oznaka)
-    #     if generic != None:
-    #         raise Exception('Genericka komponenta je u pitanju, ne upisuj je u listu')
-    #     self.kolicina = int(lista[5][1:-2])
-    #
-    #     self.duzina = locale.atof((re.search("^\d+", lista[2][1:-1])).group())
-    #     self.sirina = locale.atof((re.search("^\d+", lista[3][1:-1])).group())
-    #     self.debljina = locale.atof((re.search("^\d+", lista[4][1:-1])).group())
-    #
-    #     kant = re.search("_kant_.+$", lista[1][1:-1])
-    #     if kant != None:
-    #         kant = kant.group()
-    #         obe_kant = re.search("_obe_\d", kant)
-    #         if obe_kant != None:
-    #             obe_kant = obe_kant.group()
-    #             self.broj_kantovanih_duzina = int(obe_kant[-1])
-    #             self.broj_kantovanih_sirina = int(obe_kant[-1])
-    #         else:
-    #             duzina_kant = re.search("_duzina_\d", kant)
-    #             if duzina_kant != None:
-    #                 duzina_kant = duzina_kant.group()
-    #                 self.broj_kantovanih_duzina = int(duzina_kant[-1])
-    #             sirina_kant = re.search("_sirina_\d", kant)
-    #             if sirina_kant != None:
-    #                 sirina_kant = sirina_kant.group()
-    #                 self.broj_kantovanih_sirina = int(sirina_kant[-1])
-    #
-    #     self.duzni_metar_materijala = self.duzina * self.kolicina / 1000
-    #     self.kvadratura_materijala = self.duzina * self.sirina * self.kolicina
-    #     self.kvadratura_materijala /= 1000000
-    #     self.duzni_metar_trake = self.duzina * self.broj_kantovanih_duzina + self.sirina * self.broj_kantovanih_sirina
-    #     self.duzni_metar_trake *= self.kolicina
-    #     self.duzni_metar_trake /= 1000
-    #
-    #     List.redni_broj += 1
-    #     self.redni_broj  = List.redni_broj
-    #     List.kvadratura_materijala(self.materijal, self.kvadratura_materijala)
-    #     List.component_lista.append(self)
+    def __init__(self, line):
+        if line[0] not in lista.Lista.ulaz_csv.keys():
+            lista.Lista.ulaz_csv[line[0]] = sablon
+
+        self.__procitaj_oznaka_duzina_sirina_debljina_broj_elemenata(line)
+        self.__odredi_kantove()  # cita katnove iz oznake pa mu trebe line kao aegument
+        self.__uredi_statistiku(line)
 
     def __str__(self):
-
         return "0" + str(self.redni_broj) + ";" + \
                str(self.duzina).replace(".0", "") + ";" + \
                traka.Traka.postavi_kant(self.broj_kantovanih_duzina) + ";" + \
--- lista.py
+++ lista.py
@@ -1,5 +1,4 @@
 import materijal
-import element
 
 
 class Lista:
